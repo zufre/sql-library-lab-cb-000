dbd27b747717f4de3bb8155c0dfb3db6d16c6ed9
Cedric David <cedric.david@btopenworld.com>
2018-09-10 13:17:58 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,6 +34,7 @@ def select_series_title_with_most_human_characters
   JOIN characters
   ON series.id = characters.series_ID
   WHERE characters.species = 'Human'
+  GROUP BY series.title
   ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
