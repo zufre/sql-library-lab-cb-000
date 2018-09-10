30b7b41296f1dbf69744e5e0ee5291b4b8c22818
Cedric David <cedric.david@btopenworld.com>
2018-09-10 13:10:44 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,12 +29,12 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title 
+  "SELECT series.title
   FROM series
   JOIN character
   ON series.id = characters.series_ID
   WHERE characters.species = 'Human'
-  ORDER BY character.species DESC LINIT 1;"
+  ORDER BY character.species DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
