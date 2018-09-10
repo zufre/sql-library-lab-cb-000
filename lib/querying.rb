3cabea2d9526002378d2122206feaec698c8e238
Cedric David <cedric.david@btopenworld.com>
2018-09-10 12:44:09 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species)
+  FROM characters
+  ORDER BY COUNT(characters.species) DESC Limit 1"
 end
 
 def select_name_and_series_subgenres_of_authors
