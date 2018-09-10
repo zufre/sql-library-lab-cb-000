db6e2b5bed2cf1d88e854ca0e5fda3e6a7a3f9ab
Cedric David <cedric.david@btopenworld.com>
2018-09-10 12:46:30 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,8 +13,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species)
+  "SELECT species, COUNT(species)
   FROM characters
+  GROUP BY species
   ORDER BY COUNT(characters.species) DESC Limit 1;"
 end
 
