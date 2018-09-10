9ad87ba3c857e048afdcc919a91c0e503669f0de
Cedric David <cedric.david@btopenworld.com>
2018-09-10 13:44:37 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,7 +39,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books)
+  "SELECT characters.name, COUNT(characters.character_books)
   FROM characters
   JOIN character_books
   ON characters.id = character_books.character_id
