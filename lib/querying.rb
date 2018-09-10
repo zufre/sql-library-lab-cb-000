57504d01006266806c3ec0c77a3e0f2b09961365
Cedric David <cedric.david@btopenworld.com>
2018-09-10 13:53:28 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -46,5 +46,5 @@ def select_character_names_and_number_of_books_they_are_in
   JOIN books
   ON character_books.book_id = books.id
   GROUP BY characters.name
-  ORDER BY COUNT(*);"
+  ORDER BY COUNT(books.title);"
 end
